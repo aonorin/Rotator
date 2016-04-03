0b44b3b48952e5b48f35a1c3a508d3f18488a2a9
Dan Poblete <dan@commontown.com>
2016-04-03 22:43:33 +0800

    Reinstalled pods. Removed Swinject.

--- Pods/Target Support Files/Pods-Rotator/Pods-Rotator-frameworks.sh
+++ Pods/Target Support Files/Pods-Rotator/Pods-Rotator-frameworks.sh
@@ -84,10 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-Rotator/Swinject.framework"
   install_framework "Pods-Rotator/URLNavigator.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-Rotator/Swinject.framework"
   install_framework "Pods-Rotator/URLNavigator.framework"
 fi
